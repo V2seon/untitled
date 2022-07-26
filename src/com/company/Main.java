45a7bf729b05a8b9035b9efb08626ad17a104ccd
V2seon <101914064+V2seon@users.noreply.github.com>
2022-07-26 10:38:26 +0900

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+import com.sun.nio.sctp.SctpStandardSocketOptions;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
